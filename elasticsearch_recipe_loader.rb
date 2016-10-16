5b769fa003e5b61f3d1723cb1ac6bf1b089d7862
akulisek <akulisek@gmail.com>
2016-10-16 20:35:29 +0200

    Load data into Elasticsearch

---
+++ elasticsearch_recipe_loader.rb
@@ -0,0 +1,81 @@
+require 'HTTParty'
+require 'Pry'
+require 'csv'
+require 'json'
+
+class ElasticsearchRecipeLoader
+
+  RECIPES_PATH = './recipes2.csv'
+  ELASTICSEARCH_IP = 'http://127.0.0.1:9200'
+  ELASTICSEARCH_INDEX = '/recipes-en/recipe/'
+
+  def self.load_data
+    put_index
+
+    recipes = CSV.read(RECIPES_PATH,
+                      headers: :first_row,
+                      encoding: 'iso-8859-1:utf-8',
+                      :row_sep => :auto,
+                      :col_sep => ';'
+    )
+
+    parsed_recipes = parse_csv(recipes)
+
+    parsed_recipes.each do |recipe|
+      begin
+        response = post_recipe(recipe)
+      rescue Exception => e
+        puts "\tError: #{e}"
+      else
+        puts "\t Success: #{response}"
+      end
+    end
+
+  end
+
+  private
+
+  def self.parse_csv(recipes)
+    parsed_recipes = []
+    if recipes
+      recipes.each do |row|
+        recipe = {}
+        recipe[:name] = row['recipe name']
+        recipe[:ingredients] = row[' ingredients'].strip.gsub(/\"/, '').gsub(/, /,',').split(',')
+        recipe[:preparation] = row[' preparation'].strip.gsub(/\"/, '').gsub(/\u008CÁ/,"\u2103")
+        parsed_recipes << recipe
+      end
+    end
+    parsed_recipes
+  end
+
+  def self.post_recipe(recipe_hash)
+    puts "Posting recipe to #{ELASTICSEARCH_IP + ELASTICSEARCH_INDEX}"
+    HTTParty.post(ELASTICSEARCH_IP + ELASTICSEARCH_INDEX,
+                  :body => recipe_hash.to_json,
+                  :headers => { 'Content-Type' => 'application/json' }
+    )
+  end
+
+  def self.put_index
+    puts "PUT index #{ELASTICSEARCH_IP}"
+    HTTParty.put(ELASTICSEARCH_IP + '/recipes-en',
+                  :body => { "mappings" => {
+                                "ingredients" => {
+                                    "properties" => {
+                                        "ingredients" => {
+                                            "type" => "string",
+                                            "analyzer" => "english"
+                                        }
+                                    }
+                                }
+                            }
+
+                  }.to_json,
+                  :headers => { 'Content-Type' => 'application/json' }
+    )
+  end
+
+end
+
+ElasticsearchRecipeLoader.load_data
